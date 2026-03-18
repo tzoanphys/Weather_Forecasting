147f6477e85c771e5e9363a1817cbbfb1ee136d8
tzoanphys <tzoanphys@gmail.com>
2026-03-18 22:28:11 +0100

    again caddy issue

--- weather-ml-project/src/evaluate.py
+++ weather-ml-project/src/evaluate.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
@@ -21,26 +22,56 @@ print("\nDataset:")
 print(ds)
 
 # -----------------------------------------
+# Compute wind speed
+# -----------------------------------------
+wind_speed = np.sqrt(ds["u10"]**2 + ds["v10"]**2)
+wind_speed.name = "wind_speed"
+
+# -----------------------------------------
+# Common plotting function
+# -----------------------------------------
+def save_field_plot(data_array, title, output_path, cbar_label):
+    plt.figure(figsize=(10, 6))
+    #data_array.plot(cmap="viridis",interpolation="bilinear",cbar_kwargs={"label": cbar_label})
+
+    plot = data_array.plot(cbar_kwargs={"label": cbar_label})
+    plt.title(title, fontsize=14)
+    plt.xlabel("longitude [degrees_east]")
+    plt.ylabel("latitude [degrees_north]")
+    plt.tight_layout()
+    plt.savefig(output_path, dpi=300, bbox_inches="tight")
+    plt.close()
+    print(f"Saved plot to: {output_path}")
+
+# -----------------------------------------
+# Plot wind speed
+# -----------------------------------------
+speed_path = figures_dir / "wind_speed_belgium.png"
+save_field_plot(
+    wind_speed,
+    "Belgium 10 m Wind Speed",
+    speed_path,
+    "wind speed [m/s]"
+)
+
+# -----------------------------------------
 # Plot u10
 # -----------------------------------------
-plt.figure(figsize=(8, 5))
-ds["u10"].plot()
-plt.title("Belgium 10 m East-West Wind Component (u10)")
 u10_path = figures_dir / "u10_belgium.png"
-plt.savefig(u10_path, bbox_inches="tight")
-plt.close()
-
-print(f"Saved u10 plot to: {u10_path}")
+save_field_plot(
+    ds["u10"],
+    "Belgium 10 m East-West Wind Component (u10)",
+    u10_path,
+    "u10 [m/s]"
+)
 
 # -----------------------------------------
 # Plot v10
 # -----------------------------------------
-plt.figure(figsize=(8, 5))
-ds["v10"].plot()
-plt.title("Belgium 10 m North-South Wind Component (v10)")
 v10_path = figures_dir / "v10_belgium.png"
-plt.savefig(v10_path, bbox_inches="tight")
-plt.close()
-plt.show()
-
-print(f"Saved v10 plot to: {v10_path}")
+save_field_plot(
+    ds["v10"],
+    "Belgium 10 m North-South Wind Component (v10)",
+    v10_path,
+    "v10 [m/s]"
+)
